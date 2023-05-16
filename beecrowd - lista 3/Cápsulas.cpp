17eefbb6f82a71189b2e117718b86ab30c9c0d56
JoonMariano <marianojoao52@gmail.com>
2023-05-16 04:22:11 -0300

    binary search, ans in entry

---
+++ beecrowd - lista 3/Cápsulas.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool busca(int F, const vector<int>& capsulas, int dias) { = 12
+    long long totalCoins = 0;
+    for (int capsula : capsulas) {
+        totalCoins += dias / capsula;
+    }
+    return totalCoins >= F;
+}
+
+int main() {
+    int N, F;
+    cin >> N >> F;
+
+    vector<int> capsulas(N);
+    for (int i = 0; i < N; i++) {
+        cin >> capsulas[i];
+    }
+
+                            // busca binária 
+    long long low = 1;      // menor número de dias possível
+    long long high = 1e8;   // maior número de dias possível
+
+    while (low < high) {
+        long long mid = (low + high) / 2;
+
+        if (busca(F, capsulas, mid)) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    cout << low << endl;
+
+    return 0;
+}
