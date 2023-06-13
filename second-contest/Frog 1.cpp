b5321fc5beca556f72bcf5b806b9028621d943d0
JoonMariano <marianojoao52@gmail.com>
2023-06-13 20:20:47 -0300

    dp

---
+++ second-contest/Frog 1.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define INF 100000010
+#define ALL(x) x.begin(), x.end()
+#define UNIQUE(c) (c).resize(unique(ALL(c)) - (c).begin())
+#define REP(i, a, b) for(int i = (a); i <= (b); i++)
+#define POS(n, x) (lower_bound(ALL(n), x) - (n).begin())
+#define ll long long
+#define ld long double
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii>
+#define PB push_back
+#define EB emplace_back
+#define endl '\n'
+#define MAXN 101010
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define ordered_set tree<os_type, null_type,less<os_type>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+int minCost(vi &heights) {
+    int n = heights.size();
+
+    vi dp(n, INF);
+
+    dp[0] = 0;
+    dp[1] = abs(heights[1] - heights[0]);
+
+    for (int i = 2; i < n; i++) {
+        dp[i] = min(dp[i - 1] + abs(heights[i] - heights[i - 1]), dp[i - 2] + abs(heights[i] - heights[i - 2]));
+    }
+    
+    return dp[n - 1];
+}
+
+int main(int argc, char **argv)
+{
+    optimize;
+    
+    int N;
+
+    cin >> N;
+
+    vi heights(N);
+
+    for(int i = 0; i < N; i++)
+        cin >> heights[i];
+
+    int ans = minCost(heights);
+
+    cout << ans << endl;
+
+    return 0;
+}
